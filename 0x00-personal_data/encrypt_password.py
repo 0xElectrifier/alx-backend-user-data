0b9a2f20b8ff5838a4847bfdd4fb0eedcdd2f782
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-28 01:13:17 +0100

    Added documentation and she-bang

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+"""
+Defines a hash_password function to return a hashed password
+"""
+import bcrypt
+from bcrypt import hashpw
+
+
 def hash_password(password: str) -> bytes:
     """
     Returns a hashed password
