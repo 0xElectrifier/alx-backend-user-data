20fa72d7edde5888d53b32fc74e5523b1b257ef7
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-29 14:04:16 +0100

    Obfuscating items in a string

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,31 +1,8 @@
-#!/usr/bin/env python3
-"""
-Definition of filter_datum function that returns an obfuscated log message
-"""
-from typing import List
-import re
+#!/usr/bin/python3
+"""Module that defines a function: filter_datum for Task_0"""
 import logging
-import os
-import mysql.connector
-
-
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
-
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """
-    Return an obfuscated log message
-    Args:
-        fields (list): list of strings indicating fields to obfuscate
-        redaction (str): what the field will be obfuscated to
-        message (str): the log line to obfuscate
-        separator (str): the character separating the fields
-    """
-    for field in fields:
-        message = re.sub(field+'=.*?'+separator,
-                         field+'='+redaction+separator, message)
-    return message
+import re
+from typing import List
 
 
 class RedactingFormatter(logging.Formatter):
@@ -36,70 +13,34 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
+    def __init__(self):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """
-        redact the message of LogRecord instance
-        Args:
-        record (logging.LogRecord): LogRecord instance containing message
-        Return:
-            formatted string
-        """
-        message = super(RedactingFormatter, self).format(record)
-        redacted = filter_datum(self.fields, self.REDACTION,
-                                message, self.SEPARATOR)
-        return redacted
+        NotImplementedError
 
 
-def get_logger() -> logging.Logger:
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """Returns a log message
+
+    Args:
+        fields (list): all fields to obfuscate
+        redaction (str): a string representing by what the field will
+                         be obfuscated
+        message (str): log line
+        separator (str): character separating all fields in the log line
+    Returns:
+        Log message as string
     """
-    Return a logging.Logger object
-    """
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
+    final_message:str = message
+    for field in fields:
+        pattern: str = r"({}=)([^{}]+)".format(field, separator)
+        final_message = re.sub(pattern, r"\1" + redaction, final_message)
 
-    handler = logging.StreamHandler()
-
-    formatter = RedactingFormatter(PII_FIELDS)
-
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+    return final_message
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-    """
-    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
-    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
-    host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(user=user,
-                                   password=passwd,
-                                   host=host,
-                                   database=db_name)
-    return conn
 
-
-def main():
-    """
-    main entry point
-    """
-    db = get_db()
-    logger = get_logger()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-    fields = cursor.column_names
-    for row in cursor:
-        message = "".join("{}={}; ".format(k, v) for k, v in zip(fields, row))
-        logger.info(message.strip())
-    cursor.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    main()
