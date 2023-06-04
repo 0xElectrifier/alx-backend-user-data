7de6df02f601e36877d86777c9ad017d02514211
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-04 09:25:37 +0100

    ADded an 'is_valid' function to validate hashed_passwords with their original string value

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,3 +13,11 @@ def hash_password(password: str) -> Union[bytes | None]:
     hashed_pw = bcrypt.hashpw(pw, salt)
 
     return hashed_pw
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates @hashed_password with the provided @password"""
+    if type(hashed_password) is not bytes or type(password) is not str:
+        return False
+    dec_pw = password.encode()
+    return bcrypt.checkpw(dec_pw, hashed_password)
