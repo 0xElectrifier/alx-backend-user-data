d35ef2d62561774ba95b663fa89f45b826daa70f
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-04 09:31:39 +0100

    Type annotated the 'hash_password' function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,9 +8,9 @@ def hash_password(password: str) -> Union[bytes | None]:
     """Hashes a password"""
     if type(password) is not str:
         return None
-    pw = password.encode('utf-8')
-    salt = bcrypt.gensalt()
-    hashed_pw = bcrypt.hashpw(pw, salt)
+    pw: bytes = password.encode('utf-8')
+    salt: bytes = bcrypt.gensalt()
+    hashed_pw: bytes = bcrypt.hashpw(pw, salt)
 
     return hashed_pw
 
@@ -19,5 +19,5 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     """Validates @hashed_password with the provided @password"""
     if type(hashed_password) is not bytes or type(password) is not str:
         return False
-    dec_pw: bytes = password.encode()
+    dec_pw: bytes = password.encode("utf-8")
     return bcrypt.checkpw(dec_pw, hashed_password)
