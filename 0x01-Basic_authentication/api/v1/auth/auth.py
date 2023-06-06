b07ef5d54398bba4b51b2cd035f77593281c4bb3
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-06 11:13:29 +0100

    Attempting to resolve 'module not documented

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-""" Authenticaton
+""" Module for Authenticaton
 """
 from flask import request
-from typing import List
+from typing import (List, TypeVar)
 
 
 class Auth:
-    """ Authentication class
-    """
+    """Authentication class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Returns False
