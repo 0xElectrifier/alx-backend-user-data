f03f7897b21257778b57fe4db0197d3fd894f623
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-06 11:53:34 +0100

    Modified the 'require_auth' function: added more complex logic

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,6 +11,20 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Returns False
         """
+        if (path is None or
+                excluded_paths is None or
+                type(excluded_paths) is list and len(excluded_paths) == 0):
+            return True
+
+        slash_ends_path = path[len(path) - 1:] == '/'
+        if slash_ends_path:
+            slashed_path = path
+        else:
+            slashed_path = path + '/'
+
+        if slashed_path in excluded_paths:
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
