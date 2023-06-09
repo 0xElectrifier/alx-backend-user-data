9c89601805400e1f01b6b575f1f3c1fe964a09d0
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-09 12:30:01 +0100

    Extracting authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 """Module for Basic Auth"""
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """Basic Authentication Class"""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Returns the Base64 part of @authorization_header for
+        a Basic Authentication
+        """
+        if authorization_header is None or type(authorization_header) is not str:
+            return None
+        auth_type = re.match("^(Basic )(.*)", authorization_header)
+        if auth_type is None:
+            return None
+        auth_type = auth_type.groups()
+        if auth_type[0] != "Basic ":
+            return None
+
+        return auth_type[1]
