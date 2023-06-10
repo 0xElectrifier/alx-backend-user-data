f2f88b198b3076d8a2d52405992e793aea2f3adc
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-11 00:37:34 +0100

    Decoding a base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module for Basic Auth"""
 from api.v1.auth.auth import Auth
+import base64
 import re
 
 
@@ -24,3 +25,17 @@ class BasicAuth(Auth):
             return None
 
         return auth_type[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """Returns the decoded value of @base64_authorization_header string
+        """
+        if (base64_authorization_header is None or
+                type(base64_authorization_header) is not str):
+            return None
+        try:
+            decoded_auth = base64.b64decode(base64_authorization_header)
+        except Exception as e:
+            return None
+        return decoded_auth.decode('utf-8')
