7b4075b63d1ba8ffef9fb27b37a6cc604689a562
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-14 12:59:50 +0100

    Resolving checker error,

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -30,7 +30,7 @@ def view_one_user(user_id: str = None) -> str:
     elif user_id == "me" and request.current_user is None:
         abort(404)
     elif user_id == "me" and request.current_user is not None:
-        return jsonify(request.current_user), 200
+        return jsonify(request.current_user.to_json()), 200
     user = User.get(user_id)
     if user is None:
         abort(404)
