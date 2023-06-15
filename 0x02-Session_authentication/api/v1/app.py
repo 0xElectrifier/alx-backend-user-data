656b7f42b226c959399f1bd9724f97747bea0f90
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-15 12:11:31 +0100

    Modified @app.before_request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,10 +35,12 @@ def before_req():
                 '/api/v1/status/',
                 '/api/v1/unauthorized/',
                 '/api/v1/forbidden/',
+                '/api/v1/auth_session/login/'
         ]
         path = request.path
         if auth.require_auth(path, excluded):
-            if auth.authorization_header(request) is None:
+            if (auth.authorization_header(request) is None and
+                    auth.session_cookie(request) is None):
                 abort(401)
             request.current_user = auth.current_user(request)
             if request.current_user is None:
