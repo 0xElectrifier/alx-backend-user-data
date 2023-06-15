f79d9508c4a5bd9834b1641cc098058629065943
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-15 15:15:01 +0100

    Creating the login route

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""View for all session authentication related endpoint
+"""
+from api.v1.views import app_views
+from flask import Blueprint, jsonify
+
+
+@app_views("/auth_session/login", method=["POST"], strict_slashes=False)
+def log_in():
+    """Route for submitting logging data"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if email is None:
+        return jsonify({
+            "error": "email missing"
+            }), 400
+    if password is None:
+        return jsonify({
+            "error": "password missing"
+            }), 400
+    user_by_email = User.search({"email": email})
+    if user_by_email == []:
+        return jsonify({
+            "error": "no user found for this email"
+            }), 404
+    user_by_email = user_by_email[0]
+    if not user_by_email.is_valid_password(password):
+        return jsonify({
+            "error": "wrong password"
+            }), 401
+    else:
+        from api.v1.app import auth
+        user_id = user_by_email.id
+        session_id = auth.create_session(user_id)
+        session_name = os.getenv("SESSION_NAME")
+        out = jsonify(user_by_email.to_json())
+        out.set_cookie(session_name, session_id)
+        return out
