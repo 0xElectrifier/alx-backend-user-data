41c3fae366f949f56264d44d5218f5b91c88e9be
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-17 18:24:52 +0100

    Corrected importation error

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """User model"""
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.declarative import declarative_base
+from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -11,8 +10,8 @@ class User(Base):
     """An SQLAlchemy Database schema named 'users'"""
 
     __tablename__ = "users"
-    id = Column("id", primary_key=True)
-    email = Column("email", nullable=False)
-    hashed_password = Column("hashed_password", nullable=False)
-    session_id = Column("session_id")
-    reset_token = Column("reset_token")
+    id = Column("id", Integer, primary_key=True)
+    email = Column("email", VARCHAR(250), nullable=False)
+    hashed_password = Column("hashed_password", VARCHAR(250), nullable=False)
+    session_id = Column("session_id", VARCHAR(250))
+    reset_token = Column("reset_token", VARCHAR(250))
