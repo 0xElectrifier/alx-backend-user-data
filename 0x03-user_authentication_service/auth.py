34dd6ddacc7f9f559df6438a006015835a04a9d4
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-20 08:47:46 +0100

    Register user in auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Module for Hashed Password"""
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -9,3 +11,24 @@ def _hash_password(password: str) -> bytes:
     salt = gensalt()
     h_pw = hashpw(password.encode(), salt)
     return h_pw
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Creates a 'User' instance and saves it to the database"""
+        from sqlalchemy.orm.exc import NoResultFound
+        try:
+            user_by_email = self._db.find_user_by(email=email)
+            if user_by_email:
+                raise ValueError("User {} already exists".format(
+                    user_by_email.email))
+        except NoResultFound:
+            h_pwd = _hash_password(password)
+            new_user = self._db.add_user(email=email, hashed_password=h_pwd)
+            return new_user
