a442770b5343fd54bde96f979bb0bd9d525bd0b4
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-20 23:16:44 +0100

    Added a password validation function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """Module for Hashed Password"""
+from bcrypt import checkpw, hashpw, gensalt
 from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 from user import User
 
 
 def _hash_password(password: str) -> bytes:
     """Returns the salted hash of @password, hashed with bcrypt.hashpw
     """
-    from bcrypt import hashpw, gensalt
     salt = gensalt()
     h_pw = hashpw(password.encode(), salt)
     return h_pw
@@ -22,7 +23,6 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         """Creates a 'User' instance and saves it to the database"""
-        from sqlalchemy.orm.exc import NoResultFound
         try:
             user_by_email = self._db.find_user_by(email=email)
             if user_by_email:
@@ -32,3 +32,17 @@ class Auth:
             h_pwd = _hash_password(password)
             new_user = self._db.add_user(email=email, hashed_password=h_pwd)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Queries the database to check if @email and @password passed
+        matches existing data
+        """
+        if type(email) is not str or type(password) is not str:
+            return False
+        e_pwd = password.encode()
+        try:
+            user_by_email = self._db.find_user_by(email=email)
+            is_valid = checkpw(e_pwd, user_by_email.hashed_password)
+            return is_valid
+        except NoResultFound:
+            return False
