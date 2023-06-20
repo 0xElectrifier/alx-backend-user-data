d6d9dc5f4a92624ba21114e66808ad750c4e4d6a
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-20 10:21:33 +0100

    Moved the Auth instance declared to file global scope

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", strict_slashes=False)
@@ -10,18 +12,18 @@ def get_root():
     """Returns a simple json message"""
     return jsonify({"message": "Bienvenue"}), 200
 
+
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def users():
     """Endpoint to register a new user"""
-    from auth import Auth
-    AUTH = Auth()
     email = request.form.get("email")
     pwd = request.form.get("password")
     try:
         new_user = AUTH.register_user(email, pwd)
-        return jsonify({"email": new_user.email, "message": "user created"})
-    except ValueError:
+    except:
         return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": new_user.email,
+                    "message": "user created"}), 200
 
 
 if __name__ == "__main__":
