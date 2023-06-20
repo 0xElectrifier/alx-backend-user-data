307c4fa366d7688df3c8c3fe3101e8e5a4a6746f
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-06-20 09:50:51 +0100

    Resolving checker error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,6 +10,19 @@ def get_root():
     """Returns a simple json message"""
     return jsonify({"message": "Bienvenue"}), 200
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """Endpoint to register a new user"""
+    from auth import Auth
+    AUTH = Auth()
+    email = request.form.get("email")
+    pwd = request.form.get("password")
+    try:
+        new_user = AUTH.register_user(email, pwd)
+        return jsonify({"email": new_user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
